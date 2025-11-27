a0a3c854ee899960f3fa7605d403394b0f1a8f54
Coooder <1637120528@qq.com>
2025-11-27 19:47:08 +0800

    feat: refactor for core function

--- packages/core/src/random.ts
+++ packages/core/src/random.ts
@@ -1,5 +1,5 @@
 export function cryptoRandom() {
-  if (window.crypto?.getRandomValues) {
+  if (typeof window !== "undefined" && window.crypto?.getRandomValues) {
     const buf = new Uint32Array(2);
     window.crypto.getRandomValues(buf);
     return `${buf[0].toString(16)}${buf[1].toString(16)}`;
